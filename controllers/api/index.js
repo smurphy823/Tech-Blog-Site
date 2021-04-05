579632fcdbd952b1779c0d74c522a775808f5c2b
Stephanie Murphy <smurphy@mas.local>
2021-04-05 14:18:49 -0400

    finished handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
-const postRoutes = require('./post');
 const commentRoutes = require('./comment');
 
 router.use('/users', userRoutes);
-router.use('/post', postRoutes);
 router.use('/comment', commentRoutes);
+
 
 module.exports = router;
